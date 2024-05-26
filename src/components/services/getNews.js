7c48848ebcfa4a2a32cc80ee5aa3b6dc7fabc936
diasoy <22082010134@student.upnjatim.ac.id>
2024-05-26 20:58:57 +0700

    mod getNews

--- src/components/services/getNews.js
+++ src/components/services/getNews.js
@@ -3,11 +3,16 @@ import axios from "axios";
 const NEWS_API_BASE_URL = import.meta.env.VITE_NEWS_API_BASE_URL;
 const NEWS_API_KEY = import.meta.env.VITE_API_KEY;
 
+console.log("NEWS_API_BASE_URL:", NEWS_API_BASE_URL);
+console.log("NEWS_API_KEY:", NEWS_API_KEY);
+
 export const getNews = async ({ searchQuery = "technology" }) => {
   try {
     const path = "/everything";
     const query = `?q=${searchQuery}&apiKey=${NEWS_API_KEY}`;
     const endpointURL = `${NEWS_API_BASE_URL}${path}${query}`;
+
+    console.log("Endpoint URL:", endpointURL);
 
     const res = await axios.get(endpointURL);
 
@@ -18,8 +23,16 @@ export const getNews = async ({ searchQuery = "technology" }) => {
       const resData = { ...res.data, ...additionalObj };
 
       return resData;
+    } else {
+      console.error("Unexpected response status:", res.status);
     }
   } catch (error) {
-    console.error("Error getting Tech News: ", error);
+    if (error.response) {
+      console.error("Error response:", error.response.data);
+    } else if (error.request) {
+      console.error("Error request:", error.request);
+    } else {
+      console.error("Error message:", error.message);
+    }
   }
 };
